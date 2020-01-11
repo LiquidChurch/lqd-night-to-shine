ce0d916db1d8acfedcf13147d2fc4fe6d4edae3d
george <george@tuan.pro>
2020-01-11 14:06:52 -0500

    Add in ngx-foundation node module and fix tree-kill version

--- ngwebapp/src/app/app.module.ts
+++ ngwebapp/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AlertModule, BsDropdownModule, ModalModule } from 'ngx-foundation';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    ModalModule.forRoot(),
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
